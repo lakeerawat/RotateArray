3d137f007fd970442214d1dab501928394dcdbc2
Lakee Rawat <73211332+lakeerawat@users.noreply.github.com>
2021-12-29 23:58:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayChallenge
+{
+     class Program
+    {
+        static void RotateArray(int[] input)
+        {
+            int temp = input[0];
+            for (int i=0; i<input.Length-1; i++)
+            {
+                input[i] = input[i+1];
+            }
+            input[input.Length-1] = temp;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, };
+            RotateArray(arr);
+            Array.ForEach(arr, Console.WriteLine);
+                Console.ReadLine();
+        }
+    }
+}
